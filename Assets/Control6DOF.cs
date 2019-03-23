836eef14f5574eed475346573e6541b58a4bbf5f
Hyun Barng <harrybarng@gmail.com>
2019-03-22 17:36:18 -0700

    selecting option works

--- Assets/Control6DOF.cs
+++ Assets/Control6DOF.cs
@@ -9,29 +9,50 @@ public class Control6DOF : MonoBehaviour
     #region Private Variables
     private MLInputController _controller;
     private ControllerConnectionHandler _controllerConnectionHandler;
-    private GameObject menuEl;
-    private GameObject menuE2;
-    private GameObject menuE3;
-    private GameObject menuHolder;
-    //public List<GameObject> menuObjects;
     private const float menuScale = 0.8f;
+    private const float selScale = 1f;
     private Vector3 upVal = new Vector3(0f,0f,0.08f);
     private Vector3 downVal = new Vector3(0f, 0f, -0.08f);
     private const float speed = 0.2f;
-    
+    private GameObject selectedOpt;
+
+    private bool touchpad = false;
+
+    // default index of selElIndex is the last element of the menuObjects and the 
+    //last element should always be the "empty hand option"
+    private int selElIndex;
+    private int curIndex;
+
+    public List<GameObject> menuObjects;
+
+    private const float HALF_HOUR_IN_DEGREES = 15.0f;
+    private const float DEGREES_PER_HOUR = 12.0f / 360.0f;
+
+    private const int MIN_LED_INDEX = (int)(MLInputControllerFeedbackPatternLED.Clock12);
+    private const int MAX_LED_INDEX = (int)(MLInputControllerFeedbackPatternLED.Clock6And12);
+    private const int LED_INDEX_DELTA = MAX_LED_INDEX - MIN_LED_INDEX;
+    private int _lastLEDindex = -1;
+
     #endregion
 
     #region Unity Methods
     void Start()
     {
-        //menuObjects = new List<GameObject>();
-        menuEl = GameObject.Find("el1");
-        menuEl.transform.localScale = new Vector3(0f, 0f, 0f);
-        menuE2 = GameObject.Find("el2");
-        menuE2.transform.localScale = new Vector3(0f, 0f, 0f);
-        menuE3 = GameObject.Find("el3");
-        menuE3.transform.localScale = new Vector3(0f, 0f, 0f);
-        menuHolder = GameObject.Find("MenuHolder");
+        menuObjects = new List<GameObject>
+        {
+            GameObject.Find("el0"),
+            GameObject.Find("el1"),
+            GameObject.Find("el2"),
+            GameObject.Find("deselect")
+        };
+        selElIndex = menuObjects.Count - 1;
+        curIndex = selElIndex;
+        for (int i = 0; i < menuObjects.Count; i++)
+        {
+            menuObjects[i].transform.localScale = new Vector3(0f, 0f, 0f);
+        }
+
+        Debug.Log("Set all to 0");
         //Start receiving input by the Control
         MLInput.Start();
         _controller = MLInput.GetController(MLInput.Hand.Left);
@@ -57,12 +78,105 @@ public class Control6DOF : MonoBehaviour
         //Attach the Beam GameObject to the Control
         transform.position = _controller.Position;
         transform.rotation = _controller.Orientation;
-        
+        selectedOpt.transform.position = _controller.Position;
+        selectedOpt.transform.rotation = _controller.Orientation;
+        UpdateLED();
     }
     #endregion
 
+    private void UpdateLED()
+    {
+        if (!_controllerConnectionHandler.IsControllerValid())
+        {
+            return;
+        }
+
+        MLInputController controller = _controllerConnectionHandler.ConnectedController;
+        if (controller.Touch1Active && touchpad)
+        {
+            // Get angle of touchpad position.
+            float angle = -Vector2.SignedAngle(Vector2.up, controller.Touch1PosAndForce);
+            if (angle < 0.0f)
+            {
+                angle += 360.0f;
+            }
+
+            // Get the correct hour and map it to [0,6]
+            int index = (int)((angle + HALF_HOUR_IN_DEGREES) * DEGREES_PER_HOUR) % LED_INDEX_DELTA;
+
+            // Pass from hour to MLInputControllerFeedbackPatternLED index  [0,6] -> [MAX_LED_INDEX, MIN_LED_INDEX + 1, ..., MAX_LED_INDEX - 1]
+            index = (MAX_LED_INDEX + index > MAX_LED_INDEX) ? MIN_LED_INDEX + index : MAX_LED_INDEX;
+
+            if (_lastLEDindex != index)
+            {
+                MLInputControllerTouchpadGestureDirection dir = controller.TouchpadGesture.Direction;
+                Debug.Log(dir);
+                if (dir.ToString().Equals("Left"))
+                {
+                    Debug.Log("left selected");
+                    if(selElIndex <= 0)
+                    {
+                        selElIndex = menuObjects.Count - 2;
+                    }
+                    else
+                    {
+                        selElIndex--;
+                    }
+                    Debug.Log(selElIndex);
+                    highlightEl();
+                }
+                else if (dir.ToString().Equals("Right"))
+                {
+                    if (selElIndex >= menuObjects.Count-2)
+                    {
+                        selElIndex = 0;
+                    }
+                    else
+                    {
+                        selElIndex++;
+                    }
+                    Debug.Log(selElIndex);
+                    highlightEl();
+                }
+                else if (dir.ToString().Equals("Up"))
+                {
+                    selElIndex = menuObjects.Count - 1;
+                    Debug.Log(selElIndex);
+                    highlightEl();
+                }
+                else
+                {
+                    selElIndex = 1;
+                    Debug.Log(selElIndex);
+                    highlightEl();
+                }
+                // a duration of 0 means leave it on indefinitely
+                controller.StartFeedbackPatternLED((MLInputControllerFeedbackPatternLED)index, MLInputControllerFeedbackColorLED.BrightCosmicPurple, 0);
+                _lastLEDindex = index;
+            }
+        }
+        else if (_lastLEDindex != -1)
+        {
+            controller.StopFeedbackPatternLED();
+            _lastLEDindex = -1;
+        }
+    }
+
+    private void highlightEl()
+    {
+        Debug.Log(selElIndex + "selected, changing from " + curIndex);
+        GameObject selGB = menuObjects[selElIndex];
+        GameObject curGB = menuObjects[curIndex];
+        curGB.transform.localScale = new Vector3(menuScale, menuScale, menuScale);
+        selGB.transform.localScale = new Vector3(selScale, selScale, selScale);
+        curIndex = selElIndex;
+        _controller.StartFeedbackPatternVibe(MLInputControllerFeedbackPatternVibe.Bump, MLInputControllerFeedbackIntensity.Medium);
+    }
+
     private void HandleOnButtonDown(byte controllerId, MLInputControllerButton button)
     {
+        selectedOpt.active = false;
+        Destroy(selectedOpt, 1.0f);
         //MLInputController controller = _controllerConnectionHandler.ConnectedController;
         if (_controller != null && _controller.Id == controllerId &&
             button == MLInputControllerButton.Bumper)
@@ -72,17 +186,10 @@ public class Control6DOF : MonoBehaviour
             // Toggle UseCFUIDTransforms
             _controller.UseCFUIDTransforms = !_controller.UseCFUIDTransforms;
             Debug.Log("transforming position");
-            //menuEl.transform.Translate(0f,0f,0.07f, Space.Self);
-            //menuEl.transform.localScale = new Vector3(menuScale, menuScale, menuScale);
             //Vector3.Lerp(menuEl.transform.localScale, new Vector3(menuScale, menuScale, menuScale), curTime/Time.time)
-            //menuE2.transform.Translate(0f, 0f, 0.07f, Space.Self);
-            //menuE2.transform.localScale = new Vector3(menuScale, menuScale, menuScale);
-            //menuE3.transform.Translate(0f, 0f, 0.07f, Space.Self);
-            //menuE3.transform.localScale = new Vector3(menuScale, menuScale, menuScale);
-            MenuScaleNMove("pressed", menuEl);
-            MenuScaleNMove("pressed", menuE2);
-            MenuScaleNMove("pressed", menuE3);
-
+            MenuScaleNMove("pressed");
+            highlightEl();
+            touchpad = true;
         }
     }
 
@@ -95,15 +202,10 @@ public class Control6DOF : MonoBehaviour
             // Demonstrate haptics using callbacks.
             _controller.StartFeedbackPatternVibe(MLInputControllerFeedbackPatternVibe.ForceUp, MLInputControllerFeedbackIntensity.Medium);
             Debug.Log("resetting");
-            //menuEl.transform.Translate(0f, 0f, -0.07f, Space.Self);
-            //menuEl.transform.localScale = new Vector3(0f, 0f, 0f);
-            //menuE2.transform.Translate(0f, 0f, -0.07f, Space.Self);
-            //menuE2.transform.localScale = new Vector3(0f, 0f, 0f);
-            //menuE3.transform.Translate(0f, 0f, -0.07f, Space.Self);
-            //menuE3.transform.localScale = new Vector3(0f, 0f, 0f);
-            MenuScaleNMove("released", menuEl);
-            MenuScaleNMove("released", menuE2);
-            MenuScaleNMove("released", menuE3);
+            MenuScaleNMove("released");
+            touchpad = false;
+            selectedOpt = Instantiate(menuObjects[selElIndex]);
+            selectedOpt.transform.localScale = new Vector3(0.025f, 0.025f, 0.025f);
         }
     }
 
@@ -118,17 +220,24 @@ public class Control6DOF : MonoBehaviour
     }
 
     //statuses are: "pressed" & "released"
-    private void MenuScaleNMove(string status, GameObject element)
+    private void MenuScaleNMove(string status)
     {
         if (status.Equals("pressed"))
         {
-            element.transform.Translate(upVal * Time.deltaTime, Space.Self);
-            element.transform.localScale = new Vector3(menuScale, menuScale, menuScale);
+            for (int i = 0; i < menuObjects.Count; i++)
+            {
+                menuObjects[i].transform.Translate(upVal, Space.Self);
+                menuObjects[i].transform.localScale = new Vector3(menuScale, menuScale, menuScale);
+            }
+            
         }
         else
         {
-            element.transform.Translate(downVal * Time.deltaTime, Space.Self);
-            element.transform.localScale = new Vector3(0f, 0f, 0f);
+            for (int i = 0; i < menuObjects.Count; i++)
+            {
+                menuObjects[i].transform.Translate(downVal, Space.Self);
+                menuObjects[i].transform.localScale = new Vector3(0f, 0f, 0f);
+            }
         }
     }
 }
